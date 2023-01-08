b6bf6b379fb1155ff0ffb78267a1c07c112945c9
Dash Penning <dashpenning@gmail.com>
2023-01-07 21:48:39 -0800

    Inital commit

---
+++ index.js
@@ -0,0 +1,3 @@
+setTimeout(() => {
+    document.getElementById("lmao").innerHTML = "hello"
+}, 5000)
---
+++ serviceWorker.js
@@ -0,0 +1,22 @@
+const staticWebsite = "scouting-site-v1"
+const assets = [
+  "/",
+  "/index.html",
+  "/optix.png",
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticWebsite).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+      caches.match(fetchEvent.request).then(res => {
+        return res || fetch(fetchEvent.request)
+      })
+    )
+  })
