50659c848019645e256b2777a52f534f2a7ae013
Dash Penning <dashpenning@gmail.com>
2023-01-25 16:50:13 -0800

    active nav items

--- js/nav.js
+++ js/nav.js
@@ -4,7 +4,13 @@ async function addNav() {
     const html = await resp.text();
     document.getElementById("content").insertAdjacentHTML("beforebegin", html);
 
-    console.log(window.location.pathname)
+    var navItems = document.getElementById("nav-items")
+    for (var child of navItems.children) {
+        console.log(child.children[0].href)
+        if (child.children[0].href === window.location.href) {
+            child.children[0].classList.add("active")
+        }
+    }
 }
 
 addNav();
