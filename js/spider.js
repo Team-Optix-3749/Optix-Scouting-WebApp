82ec3c6e6d9d3a84b590192a88bdf4082d5a54d1
Dash Penning <dashpenning@gmail.com>
2023-02-14 16:45:53 -0800

    multiple teams on spider chart

--- js/spider.js
+++ js/spider.js
@@ -1,7 +1,36 @@
 const ctx = document.getElementById('chart1');
+
+const colors = {
+  'red0': 'rgb(255, 22, 71)',
+  'red1': 'rgb(255, 108, 71)',
+  'red2': 'rgb(255, 174, 71)',
+  'blue0': 'rgb(71, 52, 255)',
+  'blue1': 'rgb(71, 135, 255)',
+  'blue2': 'rgb(71, 222, 255)'
+};
+const colorsWithAlpha = {
+  'red0': 'rgba(255, 22, 71, 0.2)',
+  'red1': 'rgba(255, 108, 71, 0.2)',
+  'red2': 'rgba(255, 174, 71, 0.2)',
+  'blue0': 'rgba(71, 52, 255, 0.2)',
+  'blue1': 'rgba(71, 135, 255, 0.2)',
+  'blue2': 'rgba(71, 222, 255, 0.2)'
+}
 var curChart;
 
-const dataGeneric = {
+const createDataset = (label, data, color) => ({
+  label: label,
+  data: data,
+  fill: true,
+  backgroundColor: colorsWithAlpha[color],
+  borderColor: colors[color],
+  pointBackgroundColor: colors[color],
+  pointBorderColor: '#fff',
+  pointHoverBackgroundColor: '#fff',
+  pointHoverBorderColor: colors[color]
+})
+
+const createDataGeneric = () => ({
   labels: [
     'Teleop Points',
     'Autonomous Points',
@@ -21,10 +50,10 @@ const dataGeneric = {
     pointHoverBackgroundColor: '#fff',
     pointHoverBorderColor: 'rgb(255, 99, 132)'
   }]
-};
-const configGeneric = {
+})
+const createConfigGeneric = () => ({
   type: 'radar',
-  data: dataGeneric,
+  data: createDataGeneric(),
   options: {
     responsive: true,
     maintainAspectRatio: false,
@@ -34,30 +63,102 @@ const configGeneric = {
       }
     }
   },
-};
+})
+
+function getScores(teamNum){
+  var data = JSON.parse(localStorage.getItem("StorageData"))
+  var filteredMatches = data.filter((element) =>{
+      if (element.teamNumber.toString() == teamNum){
+          return true
+      } else return false
+  })
+  var obj = {score: 0, autoScore: 0, matches: 0, upper: 0, middle: 0, lower: 0, offense: 0, defense: 0, breakdowns: 0, teleScore: 0, balanceAuto: 0, balanceTele: 0}
+
+  for(var i of filteredMatches) {
+
+      var score = 0
+      var autoScore = 0
+      var teleScore = 0
+      var scoreValuesAuto = [6, 4, 3]
+      var scoreValuesTele = [5, 3, 2]
+      var counts = [0,0,0]
+      var chargeScoreValuesAuto = [6, 8]
+      var chargeScoreValuesTele = [10, 12]
+
+      i.events.forEach((element, index) => {
+          element.forEach(e => {
+              if(e==2){
+                  autoScore += scoreValuesAuto[index]
+                  score += scoreValuesAuto[index]
+                  counts[index]++
+              } else if (e==1){
+                  teleScore += scoreValuesTele[index]
+                  score += scoreValuesTele[index]
+                  counts[index]++
+              }
+          })
+      })
+      if(i.break){
+          obj.breakdowns++
+      }
+
+      const balAuto = i.balanced.toString().substring(0, 1)
+      const balTele = i.balanced.toString().substring(1, 2)
+      const balanceAuto = balAuto === "2" ? 12 : balAuto == "1" ? 8 : 0
+      const balanceTele = balTele === "2" ? 10 : balTele == "1" ? 6 : 0
+
+      obj.score += score
+      obj.autoScore += autoScore
+      obj.matches++
+      obj.upper += counts[0]
+      obj.middle += counts[1]
+      obj.lower += counts[2]
+      obj.offense += i.offense
+      obj.defense += i.defense
+      obj.teleScore += teleScore
+      obj.balanceAuto += balanceAuto
+      obj.balanceTele += balanceTele
+  }
+  return obj
+}
 
 document.getElementById("submit").onclick = () => {
-  const storageData = localStorage.getItem("StorageData")
-  var teamNum = document.getElementById("number").value
 
+  var teamNum0 = document.getElementById("number0").value
+  var teamNum1 = document.getElementById("number1").value
+  var teamNum2 = document.getElementById("number2").value
 
-  var scoreInfo = getScores(teamNum);
-  var avg = (i) => i/scoreInfo.matches;
-  var fields = [avg(scoreInfo.score), avg(scoreInfo.autoScore), 
-    avg(scoreInfo.offense), avg(scoreInfo.defense), 
-    avg(scoreInfo.balanceTele), avg(scoreInfo.balanceAuto)];
+  var color = 'blue'
 
-  var data = dataGeneric
-  var config = configGeneric
+  var nums = [teamNum0, teamNum1, teamNum2]
 
-  data.datasets[0].data = fields
-  data.datasets[0].label = teamNum
+  var data = createDataGeneric()
+  var config = createConfigGeneric()
+
+  data.datasets = []
+
+  nums.forEach((teamNum, i) => {
+
+    var scoreInfo = getScores(teamNum);
+
+    var avg = (p) => p/scoreInfo.matches;
+
+    var fields = [avg(scoreInfo.score), avg(scoreInfo.autoScore), 
+      avg(scoreInfo.offense), avg(scoreInfo.defense), 
+      avg(scoreInfo.balanceTele), avg(scoreInfo.balanceAuto)];
+
+    if (teamNum != '')
+    data.datasets.push(createDataset(teamNum, fields, color + i))
+
+  });
+
   config.data = data
 
   try{
     curChart.destroy()
   } catch{
-    return curChart = new Chart(ctx, config)
+    curChart = new Chart(ctx, config)
+    return
   }
   curChart = new Chart(ctx, config)
 }
